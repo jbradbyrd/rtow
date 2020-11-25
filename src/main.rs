497a65de1053c6b9beb2c9ce70c57a67cbdb69fb
J. Brad Byrd <brad@jbradbyrd.net>
2020-11-25 16:19:53 -0600

    Finished Chapter 2: Output an Image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
+    let float_width = image_width as f64;
+    let float_height = image_height as f64;
+
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining {}", j);
+        for i in 0..image_width {
+            let r = (i as f64) / float_width;
+            let g = (j as f64) / float_height;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+
+    eprintln!("\nComplete!");
 }
