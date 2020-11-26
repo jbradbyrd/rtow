5f6c76b9a82a9fcc8cf44380f66f5ce1dbd6d5d9
J. Brad Byrd <brad@jbradbyrd.net>
2020-11-26 10:12:55 -0600

    Finished Chapter 4: Rays, a Simple Camera, and Background

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 #![allow(dead_code)]
 
-mod vec3;
 mod color;
+mod ray;
+mod vec3;
 
-use vec3::*;
 use color::*;
+use ray::*;
+use vec3::*;
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
 
-    let image_width: i32 = 256;
-    let image_height: i32 = 256;
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
 
@@ -19,14 +39,16 @@ fn main() {
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining {}", j);
         for i in 0..image_width {
-            let pixel_color = Vec3::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(&r);
             write_color(&mut std::io::stdout(), pixel_color).unwrap();
         }
     }
 
-    eprintln!();
+    eprintln!("\nDone.");
 }
