5cebf124b6b10d9be53bde4a38bded58564aa047
J. Brad Byrd <brad@jbradbyrd.net>
2020-11-27 21:22:09 -0600

    Finished Chapter 7: Antialiasing

--- src/color.rs
+++ src/color.rs
@@ -1,15 +1,26 @@
 pub type Color = crate::vec3::Vec3;
 
 pub fn write_color(
-    stream: &mut dyn std::io::Write,
+    out: &mut dyn std::io::Write,
     pixel_color: Color,
+    samples_per_pixel: i32,
 ) -> Result<(), std::io::Error> {
+    let mut r = pixel_color.x();
+    let mut g = pixel_color.y();
+    let mut b = pixel_color.z();
+
+    // Divide the color by the number of samples.
+    let scale = 1.0 / samples_per_pixel as f64;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     // Write the translated [0,255] value of each color component.
     writeln!(
-        stream,
+        out,
         "{} {} {}",
-        (255.999 * pixel_color.x()) as i32,
-        (255.999 * pixel_color.y()) as i32,
-        (255.999 * pixel_color.z()) as i32
+        (256.0 * r.clamp(0.0, 0.999)) as i32,
+        (256.0 * g.clamp(0.0, 0.999)) as i32,
+        (256.0 * b.clamp(0.0, 0.999)) as i32
     )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+mod camera;
 mod color;
 mod hittable;
 mod hittable_list;
@@ -7,9 +8,11 @@ mod ray;
 mod sphere;
 mod vec3;
 
+use camera::*;
 use color::*;
 use hittable::*;
 use hittable_list::*;
+use rand::Rng;
 use ray::*;
 use sphere::*;
 use vec3::*;
@@ -29,6 +32,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // World
     let mut world = HittableList::new();
@@ -36,30 +40,24 @@ fn main() {
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
     // Render
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
+    let mut rng = rand::thread_rng();
+
     for j in (0..image_height).rev() {
         eprint!("\rScanlines remaining {}", j);
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let pixel_color = ray_color(&r, &world);
-            write_color(&mut std::io::stdout(), pixel_color).unwrap();
+            let mut pixel_color = Color::default();
+            for _ in 0..samples_per_pixel {
+                let u = (i as f64 + rng.gen_range(0.0, 1.0)) / (image_width - 1) as f64;
+                let v = (j as f64 + rng.gen_range(0.0, 1.0)) / (image_height - 1) as f64;
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            write_color(&mut std::io::stdout(), pixel_color, samples_per_pixel).unwrap();
         }
     }
 
